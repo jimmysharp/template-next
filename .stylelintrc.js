7a1a1bbb8363c937c37c30b425496cce686f355e
Ryosuke Yamada <jimmy.sharptech@gmail.com>
2024-01-05 05:06:39 +0900

    no-unsupported-browser-featuresの削除

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,20 +1,8 @@
 /** @type {import('stylelint').Config} */
 const config = {
-  plugins: ['stylelint-no-unsupported-browser-features'],
   extends: ['stylelint-config-standard-scss', 'stylelint-config-recess-order'],
   rules: {
     'selector-class-pattern': '^[a-z][a-zA-Z0-9]+$',
-    'plugin/no-unsupported-browser-features': [
-      true,
-      {
-        severity: 'warning',
-        browsers: ['> 0.5%', 'not dead'],
-        ignore: ['css-nesting'],
-        ignorePartialSupport: true,
-      },
-    ],
-    // レガシーブラウザ対応
-    'media-feature-range-notation': 'context',
   },
 };
 
