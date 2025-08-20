1590aafe18aa6e24b9a42c7f00ecfdda5d42e175
Ryosuke Yamada <jimmy.sharptech@gmail.com>
2025-08-21 01:36:24 +0900

    storybookをviteへ移行

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,11 +1,11 @@
-import type { StorybookConfig } from '@storybook/nextjs';
+import type { StorybookConfig } from '@storybook/nextjs-vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-docs'],
 
   framework: {
-    name: '@storybook/nextjs',
+    name: '@storybook/nextjs-vite',
     options: {},
   },
 };
