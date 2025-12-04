0fa97b39a431aab52853607fd525de3cae5cb567
Ryosuke Yamada <jimmy.sharptech@gmail.com>
2025-12-05 00:09:45 +0900

    Next.jsアップデート対応

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,12 @@ const withBundleAnalyzer = bundleAnalyzer({
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['localhost', '127.0.0.1'],
+  reactStrictMode: true,
   reactCompiler: true,
+  typedRoutes: true,
+  cacheComponents: true,
+  serverExternalPackages: ['pino', 'transport-stream'],
+  poweredByHeader: false,
 };
 
 export default withBundleAnalyzer(nextConfig);
