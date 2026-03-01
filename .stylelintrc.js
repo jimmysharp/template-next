464e1c1ef5c13af26694e88725dfe0e098a8c8d4
Ryosuke Yamada <jimmy.sharptech@gmail.com>
2026-03-01 21:33:46 +0900

    依存整理

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -6,22 +6,6 @@ const config = {
     // 現状コードがサンプルのためコメントアウト
     'selector-class-pattern': null,
   },
-  overrides: [
-    {
-      // 非トランスパイル対象CSS
-      files: ['public/**/*.css'],
-      plugins: ['stylelint-no-unsupported-browser-features'],
-      'plugin/no-unsupported-browser-features': [
-        true,
-        {
-          severity: 'warning',
-        },
-      ],
-      rules: {
-        'selector-class-pattern': null,
-      },
-    },
-  ],
 };
 
 export default config;
