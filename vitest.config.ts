2bc790e11d0629312625531850b82694012619c3
Ryosuke Yamada <jimmy.sharptech@gmail.com>
2026-03-03 23:58:20 +0900

    Vitest Browser Modeへの移行

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,18 +1,41 @@
 import react from '@vitejs/plugin-react-swc';
+import { playwright } from '@vitest/browser-playwright';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 
 const config = defineConfig({
-  plugins: [react(), tsconfigPaths()],
   test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['test.setup.ts'],
     coverage: {
       provider: 'istanbul',
       reporter: ['text', 'html'],
-      include: ['src/**/*.ts', 'src/**/*.tsx'],
+      include: ['src/**/*.{ts,tsx}'],
+      exclude: ['src/**/*.{test,spec}.{ts,tsx}', 'src/**/*.stories.{ts,tsx}'],
     },
+    projects: [
+      {
+        plugins: [tsconfigPaths()],
+        test: {
+          name: 'node',
+          globals: true,
+          environment: 'node',
+          include: ['src/**/*.node.{test,spec}.{ts,tsx}'],
+        },
+      },
+      {
+        plugins: [react(), tsconfigPaths()],
+        test: {
+          name: 'browser',
+          globals: true,
+          browser: {
+            enabled: true,
+            provider: playwright(),
+            instances: [{ browser: 'chromium' }],
+            headless: true,
+          },
+          include: ['src/**/*.browser.{test,spec}.{ts,tsx}'],
+        },
+      },
+    ],
   },
 });
 
